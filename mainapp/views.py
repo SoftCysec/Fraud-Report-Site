ae30f0599ccfad5902cf2e11609e23a632b4304f
SoftCysec <softengdedan@gmail.com>
2022-10-13 15:53:14 +0300

    Phase One Commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    pass
